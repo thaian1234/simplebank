ea01d0dff4a3140363ea83e77f6e7e43baff3b53
anha1234 <anacelol1234@gmail.com>
2024-08-08 17:10:41 +0700

    change env for production

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -15,10 +15,23 @@ var testDB *pgxpool.Pool
 
 func TestMain(m *testing.M) {
 	var err error
-	config, err := utils.LoadConfig("../..")
-	if err != nil {
-		log.Fatal("cannot load config file", err)
+	var config utils.Config
+
+	if os.Getenv("APP_ENV") == "production" {
+		// Load configuration from environment variables
+		config = utils.Config{
+			DBDriver:      os.Getenv("DB_DRIVER"),
+			DBSource:      os.Getenv("DB_SOURCE"),
+			ServerAddress: os.Getenv("SERVER_ADDRESS"),
+		}
+	} else {
+		// Load configuration from file for local development
+		config, err = utils.LoadConfig("../..")
+		if err != nil {
+			log.Fatal("cannot load config file", err)
+		}
 	}
+
 	ctx := context.Background()
 	testDB, err = pgxpool.New(ctx, config.DBSource)
 	if err != nil {
